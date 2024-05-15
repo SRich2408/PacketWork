31d62c4e8d59fc16a14353df2d5acc9815b769fe
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-05-15 14:31:54 -0400

    S

--- Box/BoxRunner.java
+++ Box/BoxRunner.java
@@ -3,10 +3,10 @@ public class BoxRunner {
     public static void main(String[] args){  
         Box Seb = new Box(2.0, 6.0);
         double area = Seb.getArea();
-        System.out.println("The area is" + area);
+        System.out.println("The area is " + area);
         Seb.bigger(2.0);
         double perimeter = Seb.getPerimeter();
-        System.out.println("The perimeter is" + perimeter);
+        System.out.println("The perimeter is " + perimeter);
         
         
     }
--- Circle/CircleRunner.java
+++ Circle/CircleRunner.java
@@ -4,9 +4,11 @@ public class CircleRunner {
     public static void main(String[] args){
         Circle Seb = new Circle(3);
         double circumference = Seb.getC();
-        System.out.println("The circumference is" + circumference);
-        Seb.changeR(1);
+        System.out.println("The circumference is " + circumference);
         double area = Seb.getArea();
         System.out.println("The area is " + area);
+        Seb.changeR(1);
+        double area2 = Seb.getArea();
+        System.out.println("The area is " + area2);
     }
 }
